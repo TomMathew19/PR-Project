eff714e03ca4a7af9cbc03a529aec09a1bf37027
Thomas Mathew <thomasmvempilly@gmail.com>
2025-05-12 12:57:04 +0530

    sccalc.c added

---
+++ scientific calc.c
@@ -0,0 +1,12 @@
+int thomasmultiply(int a, int b)
+{
+    return a * b;
+}
+int thomasdivide(int a, int b)
+{
+    if (b == 0)
+    {
+        return 0; // Handle division by zero
+    }
+    return a / b;
+}
